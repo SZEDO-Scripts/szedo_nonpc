4b664361243922b70af0359cfd59b528d3b11b66
SZEDO-Scripts <szedoscripts01@gmail.com>
2025-06-07 16:03:02 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'SZEDO'
+description 'NoNpc by SZEDO'
+version '1.0.0'
+
+client_scripts {
+    'client.lua',
+    'config.lua',
+}
+server_script 'server.lua'
